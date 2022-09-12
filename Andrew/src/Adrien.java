ef394a49f7287aabc452eb25ac2a1ed54a5fff8d
ajnde <ajnde@ADRIEN>
2022-09-12 15:13:17 +0200

    test adrien

---
+++ Andrew/src/Adrien.java
@@ -0,0 +1,13 @@
+
+public class Adrien {
+
+	public Adrien() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int test = 0;
+	}
+
+}
